10185cc6ff3d15dc93462e11e3dc05522e1f4898
Mansoor Khan <mansoor@incubyte.co>
2025-09-15 20:40:18 +0530

    Add custom error page
    Add loading state indicator

---
+++ app/contact/loading.tsx
@@ -0,0 +1,6 @@
+// This shows the loading state when data is fetched from an API.
+// to make this work, file naming convention must be followed.
+
+export default function LoadingState() {
+	return <h1>Loading...</h1>
+}
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,7 @@
+// This file overrides built-in error page.
+// to make this work, the file name must match
+// and it must be placed under the /app directory
+
 export default function NotFoundPage() {
 	return (
 		<div style={{ textAlign: "center", padding: "50px" }}>
